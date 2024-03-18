125d70a8a9af96f5e96c57f354c76a65bce3612f
Silvio Virtuoso Júnior <141785530+Virtuosinh0@users.noreply.github.com>
2024-03-18 19:05:21 -0300

    Add files via upload

---
+++ exercicios/aula/exercicio02/src/main/java/org/example/jogo/Bloco.java
@@ -0,0 +1,10 @@
+package org.example.jogo;
+
+public class Bloco {
+    private String Nome = "Bloco Padrão";
+    private String Textura = "Terra.png";
+
+    public Item destruir(){
+        return null;
+    }
+}
